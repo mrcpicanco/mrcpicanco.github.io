d545a29cbefe43ba638352758520dd6fdf5a0ba8
Marco Picanço <marcopicanco@live.com>
2023-08-23 14:56:59 -0300

    upgrade - currículo.

--- assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "Por que não um blog usando Jekyll?",
-        "excerpt":"O Jekyll é um gerador de sites estáticos – de fato, ele é o mais popular dentre eles. Um site estático não possui componentes rodando no lado do servidor: ele usa tecnologias web padrão (HTML5, JS, CSS) e não interage com componentes dinâmicos (um banco de dados, por exemplo). É...","categories": ["blog"],
-        "tags": ["início","artigo"],
-        "url": "/blog/welcome-to-jekyll/",
-        "teaser": null
-      }]
